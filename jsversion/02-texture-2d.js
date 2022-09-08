b651e061488d3db4d8f812eacd058624acd2c2ab
ttom921 <ttom921@hotmail.com>
2022-09-08 15:44:01 +0800

    day 7 - linear-mipmap-linear

--- jsversion/02-texture-2d.js
+++ jsversion/02-texture-2d.js
@@ -44,7 +44,7 @@ async function main() {
     const textureUniformLocation = gl.getUniformLocation(program, 'u_texture');
 
     //image process
-    const image = await loadImage('https://i.imgur.com/vryPVknh.jpg');
+    const image = await loadImage('https://i.imgur.com/ISdY40yh.jpg');
     const texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(
@@ -55,8 +55,9 @@ async function main() {
         gl.UNSIGNED_BYTE,//type
         image,//data
     );
-    //gl.generateMipmap(gl.TEXTURE_2D);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
